837d55420ddf23e1ebd61ec7ec05ffb9fcb647f7
Rukotana Freddy <rukotanafreddy@gmail.com>
2021-07-06 23:41:03 +0200

    fix and processing project

--- script.js
+++ script.js
@@ -53,7 +53,13 @@ function addTransactionDOM(transaction){
     item.classList.add(transaction.amount < 0 ? 'minus' : 'plus'); 
     
     item.innerHTML = `
-        ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span><button class="delete-btn">x</button>
+        ${transaction.text}
+         <span>
+            ${sign}${Math.abs(transaction.amount)}
+        </span>
+        <button class="delete-btn" onclick="removeTransaction(${transaction.id})">
+        x
+        </button>
     `;
     list.appendChild(item);
 }
